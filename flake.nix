f785c38b54ad9f0a86199835e802d1e20ad70cad
AKYIL <141535206+AKYlU@users.noreply.github.com>
2025-04-25 20:23:54 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "newm - Wayland compositor";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # pacotes base
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils"; # utilitários para flakes
 
-    pywmpkg.url = "github:jbuchermn/pywm";
-    pywmpkg.inputs.nixpkgs.follows = "nixpkgs";
+    pywmpkg.url = "github:jbuchermn/pywm"; # biblioteca pywm
+    pywmpkg.inputs.nixpkgs.follows = "nixpkgs"; # sincroniza nixpkgs
   };
 
   outputs = { self, nixpkgs, pywmpkg, flake-utils }:
@@ -20,38 +20,34 @@
           (self: super: rec {
             python3 = super.python3.override {
               packageOverrides = self1: super1: {
-                pywm = pywmpkg.packages.${system}.pywm;
+                pywm = pywmpkg.packages.${system}.pywm; # pywm importado
+
                 dasbus = super1.buildPythonPackage rec {
                   pname = "dasbus";
                   version = "1.6";
-
                   src = super1.fetchPypi {
                     inherit pname version;
                     sha256 = "sha256-FJrY/Iw9KYMhq1AVm1R6soNImaieR+IcbULyyS5W6U0=";
                   };
-
                   setuptoolsCheckPhase = "true";
-
-                  propagatedBuildInputs = with super1; [ pygobject3 ];
+                  propagatedBuildInputs = with super1; [ pygobject3 ]; # depende de pygobject3
                 };
 
                 thefuzz = super1.buildPythonPackage rec {
                   pname = "thefuzz";
                   version = "0.19.0";
-
                   src = super1.fetchPypi {
                     inherit pname version;
                     sha256 = "sha256-b3Em2y8silQhKwXjp0DkX0KRxJfXXSB1Fyj2Nbt0qj0=";
                   };
-
-                  propagatedBuildInputs = with super1; [ 
+                  propagatedBuildInputs = with super1; [
                     python-Levenshtein
                     pycodestyle
                   ];
                 };
               };
             };
-            python3Packages = python3.pkgs;
+            python3Packages = python3.pkgs; # atalho para python3Packages
           })
         ];
       };
@@ -66,15 +62,22 @@
 
           propagatedBuildInputs = with pkgs.python3Packages; [
             pywm
-
             pycairo
             psutil
             python-pam
             pyfiglet
             dasbus
             thefuzz
-
             setuptools
+          ];
+
+          nativeBuildInputs = [
+            pkgs.meson       # meson necessário para buildar pywm
+            pkgs.ninja       # ninja para compilar rápido
+            pkgs.pkg-config  # descobrir libs
+            pkgs.cmake       # cmake para wlroots
+            pkgs.wayland     # depende do protocolo wayland
+            pkgs.libdrm      # libdrm que wlroots precisa
           ];
 
           setuptoolsCheckPhase = "true";
@@ -84,14 +87,12 @@
         my-python = pkgs.python3;
         python-with-my-packages = my-python.withPackages (ps: with ps; [
           pywm
-
           pycairo
           psutil
           python-pam
           pyfiglet
           dasbus
           thefuzz
-
           python-lsp-server
           (pylsp-mypy.overrideAttrs (old: { pytestCheckPhase = "true"; }))
           mypy
