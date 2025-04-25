238ae2cbeddc4e953d6d9cf0688508e20f2bdc24
AKYIL <141535206+AKYlU@users.noreply.github.com>
2025-04-25 20:27:55 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,11 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # pacotes base
-
-    flake-utils.url = "github:numtide/flake-utils"; # utilitários para flakes
-
-    pywmpkg.url = "github:jbuchermn/pywm"; # biblioteca pywm
-    pywmpkg.inputs.nixpkgs.follows = "nixpkgs"; # sincroniza nixpkgs
+    flake-utils.url = "github:numtide/flake-utils";       # utilitários
+    pywmpkg.url = "github:jbuchermn/pywm";                 # pywm externo
+    pywmpkg.inputs.nixpkgs.follows = "nixpkgs";            # mesmo nixpkgs
   };
 
   outputs = { self, nixpkgs, pywmpkg, flake-utils }:
@@ -20,7 +18,23 @@
           (self: super: rec {
             python3 = super.python3.override {
               packageOverrides = self1: super1: {
-                pywm = pywmpkg.packages.${system}.pywm; # pywm importado
+                # pywm reempacotado com deps extras
+                pywm = super1.buildPythonPackage rec {
+                  pname = "pywm";
+                  version = "0.3alpha";
+
+                  src = pywmpkg.packages.${system}.pywm.src;
+
+                  format = "pyproject";
+
+                  nativeBuildInputs = with super1; [
+                    meson ninja pkg-config cmake
+                  ];
+
+                  buildInputs = with super1; [
+                    wayland libdrm
+                  ];
+                };
 
                 dasbus = super1.buildPythonPackage rec {
                   pname = "dasbus";
@@ -30,7 +44,7 @@
                     sha256 = "sha256-FJrY/Iw9KYMhq1AVm1R6soNImaieR+IcbULyyS5W6U0=";
                   };
                   setuptoolsCheckPhase = "true";
-                  propagatedBuildInputs = with super1; [ pygobject3 ]; # depende de pygobject3
+                  propagatedBuildInputs = with super1; [ pygobject3 ];
                 };
 
                 thefuzz = super1.buildPythonPackage rec {
@@ -47,61 +61,33 @@
                 };
               };
             };
-            python3Packages = python3.pkgs; # atalho para python3Packages
+            python3Packages = python3.pkgs; # atalho para evitar repetição
           })
         ];
       };
     in
     {
-      packages.newm =
-        pkgs.python3.pkgs.buildPythonApplication rec {
-          pname = "newm";
-          version = "0.3alpha";
-
-          src = ./.;
-
-          propagatedBuildInputs = with pkgs.python3Packages; [
-            pywm
-            pycairo
-            psutil
-            python-pam
-            pyfiglet
-            dasbus
-            thefuzz
-            setuptools
-          ];
-
-          nativeBuildInputs = [
-            pkgs.meson       # meson necessário para buildar pywm
-            pkgs.ninja       # ninja para compilar rápido
-            pkgs.pkg-config  # descobrir libs
-            pkgs.cmake       # cmake para wlroots
-            pkgs.wayland     # depende do protocolo wayland
-            pkgs.libdrm      # libdrm que wlroots precisa
-          ];
-
-          setuptoolsCheckPhase = "true";
-        };
+      packages.newm = pkgs.python3Packages.buildPythonApplication rec {
+        pname = "newm";
+        version = "0.3alpha";
+        src = ./.;
+        propagatedBuildInputs = with pkgs.python3Packages; [
+          pywm pycairo psutil python-pam pyfiglet dasbus thefuzz setuptools
+        ];
+        setuptoolsCheckPhase = "true";
+      };
 
       devShell = let
         my-python = pkgs.python3;
         python-with-my-packages = my-python.withPackages (ps: with ps; [
-          pywm
-          pycairo
-          psutil
-          python-pam
-          pyfiglet
-          dasbus
-          thefuzz
+          pywm pycairo psutil python-pam pyfiglet dasbus thefuzz
           python-lsp-server
           (pylsp-mypy.overrideAttrs (old: { pytestCheckPhase = "true"; }))
-          mypy
-          yappi
+          mypy yappi
         ]);
-      in
-        pkgs.mkShell {
-          buildInputs = [ python-with-my-packages ];
-        };
+      in pkgs.mkShell {
+        buildInputs = [ python-with-my-packages ];
+      };
     }
   );
 }
