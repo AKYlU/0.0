a1c22cc2633f06f2e34033c9311dcc8051934877
AKYIL <141535206+AKYlU@users.noreply.github.com>
2025-04-25 00:34:25 -0300

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -2,95 +2,105 @@
   description = "newm - Wayland compositor";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";  # base do sistema
-    flake-utils.url = "github:numtide/flake-utils";        # utilitários p/ multiplataforma
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    pywmpkg.url = "github:jbuchermn/pywm";                 # dependência externa
-    pywmpkg.inputs.nixpkgs.follows = "nixpkgs";            # usa mesmo nixpkgs
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pywmpkg.url = "github:jbuchermn/pywm";
+    pywmpkg.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, pywmpkg, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: rec {
-              # Override do python3 com pacotes extras
-              python3 = super.python3.override {
-                packageOverrides = self1: super1: {
-                  pywm = pywmpkg.packages.${system}.pywm;
+  flake-utils.lib.eachDefaultSystem (
+    system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (self: super: rec {
+            python3 = super.python3.override {
+              packageOverrides = self1: super1: {
+                pywm = pywmpkg.packages.${system}.pywm;
+                dasbus = super1.buildPythonPackage rec {
+                  pname = "dasbus";
+                  version = "1.6";
 
-                  dasbus = super1.buildPythonPackage rec {
-                    pname = "dasbus";
-                    version = "1.6";
-                    src = super1.fetchPypi {
-                      inherit pname version;
-                      sha256 = "sha256-FJrY/Iw9KYMhq1AVm1R6soNImaieR+IcbULyyS5W6U0=";
-                    };
-                    doCheck = true;
-                    propagatedBuildInputs = with super1; [ pygobject3 ];
+                  src = super1.fetchPypi {
+                    inherit pname version;
+                    sha256 = "sha256-FJrY/Iw9KYMhq1AVm1R6soNImaieR+IcbULyyS5W6U0=";
                   };
 
-                  thefuzz = super1.buildPythonPackage rec {
-                    pname = "thefuzz";
-                    version = "0.19.0";
-                    src = super1.fetchPypi {
-                      inherit pname version;
-                      sha256 = "sha256-b3Em2y8silQhKwXjp0DkX0KRxJfXXSB1Fyj2Nbt0qj0=";
-                    };
-                    propagatedBuildInputs = with super1; [
-                      python-Levenshtein
-                      pycodestyle
-                    ];
+                  setuptoolsCheckPhase = "true";
+
+                  propagatedBuildInputs = with super1; [ pygobject3 ];
+                };
+
+                thefuzz = super1.buildPythonPackage rec {
+                  pname = "thefuzz";
+                  version = "0.19.0";
+
+                  src = super1.fetchPypi {
+                    inherit pname version;
+                    sha256 = "sha256-b3Em2y8silQhKwXjp0DkX0KRxJfXXSB1Fyj2Nbt0qj0=";
                   };
+
+                  propagatedBuildInputs = with super1; [ 
+                    python-Levenshtein
+                    pycodestyle
+                  ];
                 };
               };
-
-              python3Packages = python3.pkgs;  # alias p/ pacotes
-            })
-          ];
-        };
-      in
-      {
-        # Pacote exposto como "newm"
-        packages.newm = pkgs.python3Packages.buildPythonApplication rec {
+            };
+            python3Packages = python3.pkgs;
+          })
+        ];
+      };
+    in
+    {
+      packages.newm =
+        pkgs.python3.pkgs.buildPythonApplication rec {
           pname = "newm";
           version = "0.3alpha";
+
           src = ./.;
-          doCheck = true;
 
           propagatedBuildInputs = with pkgs.python3Packages; [
             pywm
+
             pycairo
             psutil
             python-pam
             pyfiglet
             dasbus
             thefuzz
+
             setuptools
           ];
+
+          setuptoolsCheckPhase = "true";
         };
 
-        # Shell de desenvolvimento com LSP e utilitários
-        devShell = let
-          my-python = pkgs.python3;
-          python-with-my-packages = my-python.withPackages (ps: with ps; [
-            pywm
-            pycairo
-            psutil
-            python-pam
-            pyfiglet
-            dasbus
-            thefuzz
-            python-lsp-server
-            (pylsp-mypy.overrideAttrs (old: { pytestCheckPhase = "true"; }))
-            mypy
-            yappi
-          ]);
-        in pkgs.mkShell {
+      devShell = let
+        my-python = pkgs.python3;
+        python-with-my-packages = my-python.withPackages (ps: with ps; [
+          pywm
+
+          pycairo
+          psutil
+          python-pam
+          pyfiglet
+          dasbus
+          thefuzz
+
+          python-lsp-server
+          (pylsp-mypy.overrideAttrs (old: { pytestCheckPhase = "true"; }))
+          mypy
+          yappi
+        ]);
+      in
+        pkgs.mkShell {
           buildInputs = [ python-with-my-packages ];
         };
-      }
-    );
+    }
+  );
 }
